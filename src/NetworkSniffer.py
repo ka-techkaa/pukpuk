94c684025a321347d4a2812ecd89a77ae97a6982
ka-techkaa <kate.cherkasina@gmail.com>
2025-10-24 16:44:58 +0300

    kek

--- src/NetworkSniffer.py
+++ src/NetworkSniffer.py
@@ -1,5 +1,6 @@
 from scapy.all import sniff, get_if_list, IP, TCP, UDP
 import time
+import threading 
 
 class NetworkSniffer:
     def __init__(self):
@@ -7,6 +8,7 @@ class NetworkSniffer:
         self.is_running = False
         self.packet_count = 1
         self.oshibka = False
+        self.sniffer_thread = None
 
     def _process_packet(self, packet):
         if not self.is_running:
@@ -35,15 +37,25 @@ class NetworkSniffer:
                 self.packets.append(packet_info)
 
 
-    def start(self, packet_count, interface=None):
+    def start(self, packet_count = None, interface=None):
         if not self.check_interface(interface) and interface != None: 
             print(f"\nche ta ne rabotaet, problema tyt {interface}\n")
             return False
         try:
             self.is_running = True
-            sniff(count = packet_count, 
-                  iface = interface,
-                  prn = self._process_packet)
+
+            if packet_count != None:
+                 sniff(count = packet_count, 
+                    iface = interface,
+                    prn = self._process_packet)
+            else:
+                self.sniffer_thread = threading.Thread(
+                    target = self._sniff_packets,
+                    args = (interface, self._process_packet),
+                    daemon = True 
+                )
+                self.sniffer_thread.start()
+
             print("zapusk") 
             return True
         except Exception as e:
@@ -61,15 +73,24 @@ class NetworkSniffer:
         except Exception as e:
             print(f"gfgff{e}")
         return []
+    
+    def _sniff_packets(self, iface, prn):
+        sniff(
+            iface = iface,
+            prn = prn)
 
 
     def stop(self):
         self.is_running = False
+        if self.sniffer_thread != None:
+            self.sniffer_thread.join(timeout=2.0)
         print("tutu")
 
 if __name__ == "__main__":
     a = NetworkSniffer()
-    a.start(16)
+    a.start()
+    time.sleep(1000.0)
+    a.stop()
     print(a.packets)
        
         
